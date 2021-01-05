f587e6a1c36dcdf0a18e56db64ed0e9a293fe7b2
Hieu Phan <64969360+Pogchop@users.noreply.github.com>
2021-01-05 21:50:48 +0700

    Add files via upload

---
+++ Lab4ex1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4ex1
+{
+    class lmao
+    {
+        int code;
+        string mename;
+        string maname;
+        int price;
+        int quantityon;
+        int quantitysold;
+        DateTime manu;
+        DateTime exp;
+        int number;
+        int plannedsales;
+        int actualsales;
+        string region;
+        public lmao(int code, string mename, string maname, int price, int quantityon, int number)
+        {
+            this.code = code;
+            this.mename = mename;
+            this.maname = maname;
+            this.price = price;
+            this.quantityon = quantityon;
+            this.number = number;
+        }
+        public void Display()
+        {
+            Console.WriteLine(code);
+            Console.WriteLine(mename);
+            Console.WriteLine(maname);
+            Console.WriteLine(price);
+            Console.WriteLine(quantityon);
+            Console.WriteLine(manu);
+            Console.WriteLine(exp);
+            Console.WriteLine(number);
+        }
+    }
+    class Lab4ex1
+    {
+        static void Main(string[] args)
+        {
+            lmao objmedicine = new lmao(1234, "dAl", "lol", 69, 22, 6969);
+            objmedicine.Display();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4ex2
+{
+    class product
+    {
+        private int productid;
+        private string productname;
+        private float price;
+        private int stock;
+        public product(int id, string name, float price, int stock)
+        {
+            productid = id;
+            productname = name;
+            this.price = price;
+            this.stock = stock;
+        }
+        public void Display()
+        {
+            Console.WriteLine(productid);
+            Console.WriteLine(productname);
+            Console.WriteLine(price);
+            Console.WriteLine(stock);
+        }
+        static void Main(string[] args)
+        {
+            product objstudent = new product(111, "YEet", 420, 69);
+            objstudent.Display();
+            Console.ReadKey();
+        }
+    }
+}
